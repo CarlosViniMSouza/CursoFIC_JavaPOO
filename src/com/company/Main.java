ce95bf083a5e191542a59b9de06c648788e43fe7
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-24 22:22:08 -0300

    projeto JavaPOO add

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Salve Salve Java! \nEh bom vê-lo novamente!");
+        // project java for FIC Course add.
+    }
+}
