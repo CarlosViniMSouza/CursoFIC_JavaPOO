6a3c2417d4162e2eb7b0823ffdcf0c1e7ca4474a
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-27 10:53:07 -0300

    Java W3School Started

---
+++ src/Java_Tutorial/Lesson01_JavaIntro.java
@@ -0,0 +1,36 @@
+package Java_Tutorial;
+
+public class Lesson01_JavaIntro {
+
+    public static void main(String []args){
+        /*
+            What is Java?
+            Java is a popular programming language, created in 1995.
+
+            It is owned by Oracle, and more than 3 billion devices run Java.
+
+            It is used for:
+
+            1- Mobile applications (specially Android apps)
+            2- Desktop applications
+            3- Web applications
+            4- Web servers and application servers
+            5- Games
+            6- Database connection
+            And much, much more!
+
+            Why Use Java?
+
+            1- Java works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc.)
+            2- It is one of the most popular programming language in the world
+            3- It is easy to learn and simple to use
+            4- It is open-source and free
+            5- It is secure, fast and powerful
+            6- It has a huge community support (tens of millions of developers)
+
+            Java is an object oriented language which gives a clear structure to programs and
+            allows code to be reused, lowering development costs
+            As Java is close to C++ and C#, it makes it easy for programmers to switch to Java or vice versa
+         */
+    }
+}
