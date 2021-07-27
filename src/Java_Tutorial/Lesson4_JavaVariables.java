aebf045eb2714e00446471247546e4281234fcfd
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-27 17:38:55 -0300

    lesson4 finish

--- src/Java_Tutorial/Lesson4_JavaVariables.java
+++ src/Java_Tutorial/Lesson4_JavaVariables.java
@@ -16,6 +16,42 @@ public class Lesson4_JavaVariables {
                 3- float - stores floating point numbers, with decimals, such as 19.99 or -19.99
                 4- char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
                 5- boolean - stores values with two states: true or false
+
+                NOTE: Just like we do in the C language, we first declare the variable's type,
+                name the variable, and then insert its contents. Example:
+
+                ```
+                int num = 10;
+                String job = "Developer";
+                boolean result = false;
+                ```
+
+                NOTE: if you assign a new value to an existing variable,
+                it will overwrite the previous value. Example:
+
+                ```
+                int value = 20;
+                value = 10;
+                System.out.println(value);   // Output: 10
+                ```
+
+                NOTE: The 'final' global word makes your variable immutable (its value cannot be
+                changed during code execution). Example:
+
+                ```
+                final float number = 9.99f;   // the variable 'number' will be '9.99' always!
+                System.out.println(number)
+                ```
+
+                ** Declare Many Variables **
+
+                To declare more than one variable of the same type, use a comma-separated list. Example:
+
+                ```
+                int x = 5, y = 10, z = 2;
+                System.out.println(x * y * z);   // Output: 100
+                ```
          */
+
     }
 }
