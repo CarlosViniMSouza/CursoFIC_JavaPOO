950fa3770538ceb0cf950098054a81ccea9b0efb
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-28 19:28:22 -0300

    lessons9 ok

---
+++ src/Java_Tutorial/Lesson9_JavaMath.java
@@ -0,0 +1,33 @@
+package Java_Tutorial;
+
+public class Lesson9_JavaMath {
+
+    public static void main(String []args) {
+
+        /*
+            __ Java Math __
+
+            The Java Math class has many methods that allows you to perform mathematical tasks on numbers.
+
+            __ Math.max(x,y) __
+
+            The Math.max(x,y) method can be used to find the highest value of x and y:
+
+            __ Math.min(x,y) __
+
+            The Math.min(x,y) method can be used to find the lowest value of x and y:
+
+            __ Math.sqrt(x) __
+
+            The Math.sqrt(x) method returns the square root of x:
+
+            __ Math.abs(x) __ --> abs : absolute
+
+            The Math.abs(x) method returns the absolute (positive) value of x:
+
+            __ Random Numbers __
+
+            Math.random() returns a random number between 0.0 (inclusive), and 1.0 (exclusive):
+         */
+    }
+}
