ae82997e020421391206edbfe02745dd41597446
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-31 15:59:46 -0300

    File Welcome add

---
+++ src/Welcome.java
@@ -0,0 +1,9 @@
+public class Welcome {
+
+    public static void main(String []args) {
+
+        // Doing 'Hello Java' with 1 line:
+        System.out.println("Hello Carlos, Welcome to Java");
+
+    }
+}
