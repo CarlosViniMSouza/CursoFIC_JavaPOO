7a6e95d6006a1095d6cce16875bdf67d4c329ccd
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-09-15 13:55:31 -0300

    Lesson: Java OOP --> add

---
+++ src/Java_OOP/Lesson01_JavaOPP_Intro.java
@@ -0,0 +1,43 @@
+package Java_OOP;
+
+public class Lesson01_JavaOPP_Intro {
+
+    /*
+    --> Java - What is OOP?
+
+    OOP stands for Object-Oriented Programming.
+
+    Procedural programming is about writing procedures or methods that perform operations on the data,
+    while object-oriented programming is about creating objects that contain both data and methods.
+
+    Advantages:
+
+    1 - OOP is faster and easier to execute;
+    2 - OOP provides a clear structure for the programs;
+    3 - OOP helps to keep the Java code DRY "Don't Repeat Yourself",
+    and makes the code easier to maintain, modify and debug;
+    4 - OOP makes it possible to create full reusable applications with less code and shorter development time;
+
+    NOTE: The "Don't Repeat Yourself" (DRY) principle is about reducing the repetition of code.
+    You should extract out the codes that are common for the application,
+    and place them at a single place and reuse them instead of repeating it.
+
+    --> Java - What are Classes and Objects?
+
+    Classes and objects are the two main aspects of object-oriented programming.
+
+    Look at the following illustration to see the difference between class and objects:
+
+    {
+        Class: Horti-fruit;
+        Objects: Banana, Apple, Strawberries, Mango
+    }
+
+    So, a class is a template for objects, and an object is an instance of a class.
+
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
