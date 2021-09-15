a9481b6ea438136510aadc4651de99891aa810f4
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-09-15 09:42:07 -0300

    Lesson: Methods Overload --> add

---
+++ src/Java_Methods/Lesson02_Method_Overl.java
@@ -0,0 +1,37 @@
+package Java_Methods;
+
+public class Lesson02_Method_Overl {
+
+    /*
+    --> Method Overloading
+
+    With method overloading, multiple methods can have the same name with different parameters. Example:
+
+    int myMethod(int x)
+    float myMethod(float x)
+    double myMethod(double x, double y)
+
+    ** Instead of defining two methods that should do the same thing, it is better to overload one. Example: **
+
+     */
+
+    static int Sum(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    static double Sum(double v1, double v2) {
+        return v1 + v2;
+    }
+
+    // Now, for use the methods, we do:
+    public static void main(String []args) {
+
+        int test1 = Sum(7, 5);
+        double test2 = Sum(5.5, 10.55);
+        System.out.println("The First result is: " + test1);
+        System.out.println("The second result is: " + test2);
+
+    }
+
+    // Note: Multiple methods can have the same name as long as the number and/or type of parameters are different.
+}
